5236767ee7cc9d6315ef01d145976eee94d5a34c
nallicock <nicholasallicock@outlook.com>
2026-08-05 17:46:01 -0400

    created Product repository/interface

---
+++ StockHQ.API/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using StockHQ.API.Models;
+
+namespace StockHQ.API.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByAsync(int id);
+        Task<Product> AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
