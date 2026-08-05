d1b77a516422aa77cd7767aafb7875bbc5a0a339
nallicock <nicholasallicock@outlook.com>
2026-08-05 17:55:24 -0400

    added Product repository

--- StockHQ.API/Interfaces/IProductRepository.cs
+++ StockHQ.API/Interfaces/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace StockHQ.API.Interfaces
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetAllAsync();
-        Task<Product?> GetByAsync(int id);
+        Task<Product?> GetByIdAsync(int id);
         Task<Product> AddAsync(Product product);
         Task UpdateAsync(Product product);
         Task DeleteAsync(int id);
---
+++ StockHQ.API/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockHQ.API.Data;
+using StockHQ.API.Interfaces;
+using StockHQ.API.Models;
+
+namespace StockHQ.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StockHQDbContext _context;
+
+        public ProductRepository(StockHQDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<Product> AddAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
