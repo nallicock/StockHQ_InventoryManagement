48f4a41a9fe7cfaec4c9f49c9655a3341b580311
nallicock <nicholasallicock@outlook.com>
2026-08-04 23:40:56 -0400

    add dbcontext

---
+++ StockHQ.API/Data/StockHQDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockHQ.API.Models;
+
+namespace StockHQ.API.Data
+{
+    public class StockHQDbContext : DbContext
+    {
+        public StockHQDbContext(DbContextOptions<StockHQDbContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
