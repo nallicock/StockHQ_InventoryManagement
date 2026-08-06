5f817e74308553a753c2559b2d7ceac225236ccb
nallicock <nicholasallicock@outlook.com>
2026-08-06 01:41:51 -0400

    added category FK to Product

--- StockHQ.API/Models/Product.cs
+++ StockHQ.API/Models/Product.cs
@@ -8,6 +8,9 @@
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int QuantityInStock { get; set; }
+        public int CategoryId { get; set; }
+        //add navigation property to know category's name
+        public Category? Category { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     }
