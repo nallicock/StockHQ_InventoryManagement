e9468360982329611882b98fb44857f8199fe0e4
nallicock <nicholasallicock@outlook.com>
2026-08-06 20:48:50 -0400

    completed categoriescontroller.. silly me

--- StockHQ.API/Controllers/CategoriesController.cs
+++ StockHQ.API/Controllers/CategoriesController.cs
@@ -22,5 +22,39 @@ namespace StockHQ.API.Controllers
 
             return Ok(categories);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            await _categoryService.UpdateCategoryAsync(category);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+
+            return NoContent();
+        }
     }
 }
