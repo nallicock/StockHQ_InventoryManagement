cf10d0b780ad57b236357a58ba9dd06eaeb6a468
nallicock <nicholasallicock@outlook.com>
2026-08-07 11:06:34 -0400

    added more comments to InventoryTransaction

--- StockHQ.API/Models/InventoryTransaction.cs
+++ StockHQ.API/Models/InventoryTransaction.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 
 namespace StockHQ.API.Models
 {
@@ -6,14 +7,20 @@ namespace StockHQ.API.Models
     {
         public int Id { get; set; }
 
+
+        //which product changed?
         [Required]
         public int ProductId { get; set; }
 
+        //Navigation property back to the product -- used with Include()
         public Product? Product { get; set; }
 
+        //how much of the quantity changed after the transaction?
         [Required]
         public int QuantityChanged { get; set; }
 
+        //why did the inventory change?
+        //sold? damaged? returned? adjustment? received?
         [Required]
         [StringLength(50)]
         public string Reason { get; set; } = string.Empty;
