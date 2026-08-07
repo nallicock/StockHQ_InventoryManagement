72388f490a6a0bac947c6ea928fed44b62644298
nallicock <nicholasallicock@outlook.com>
2026-08-07 18:55:29 -0400

    added IAuthService interface

---
+++ StockHQ.API/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StockHQ.API.Interfaces
+{
+    //controller > authservice > asp.net identity > dbcontext > sql server
+    public interface IAuthService
+    {
+        Task<IdentityResult> RegisterAsync(string email, string password);
+    }
+}
