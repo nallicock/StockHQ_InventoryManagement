3bbb62ffe0a6378c1761b58c76ebfcb5d5925ef1
nallicock <nicholasallicock@outlook.com>
2026-08-07 11:18:09 -0400

    added inventorytransaction repository interface

---
+++ StockHQ.API/Interfaces/IInventoryTransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using StockHQ.API.Models;
+
+namespace StockHQ.API.Interfaces
+{
+    public interface IInventoryTransactionRepository
+    {
+        //only need add and get by id because we should not be able to delete or edit a transaction
+        Task AddAsync(InventoryTransaction transaction);
+        Task<IEnumerable<InventoryTransaction>> GetByProductIdAsync(int productId);
+
+    }
+}
