e0e529f0e8002ad2bdeb8cac1aeafea77c7b7eb2
nallicock <nicholasallicock@outlook.com>
2026-08-07 11:49:52 -0400

    added transaction service

---
+++ StockHQ.API/Services/InventoryTransactionService.cs
@@ -0,0 +1,25 @@
+﻿using StockHQ.API.Interfaces;
+using StockHQ.API.Models;
+
+namespace StockHQ.API.Services
+{
+    public class InventoryTransactionService : IInventoryTransactionService
+    {
+        private readonly IInventoryTransactionRepository _repository;
+
+        public InventoryTransactionService(IInventoryTransactionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task AddTransactionAsync(InventoryTransaction transaction)
+        {
+            await _repository.AddAsync(transaction);
+        }
+
+        public async Task<IEnumerable<InventoryTransaction>> GetProductHistoryAsync(int productId)
+        {
+            return await _repository.GetByProductIdAsync(productId);
+        }
+    }
+}
