3debe6c990ed97062e93d60130fb04c1d61174d6
nallicock <nicholasallicock@outlook.com>
2026-08-07 11:46:09 -0400

    added inventorytransactionservice interface

---
+++ StockHQ.API/Interfaces/IInventoryTransactionService.cs
@@ -0,0 +1,11 @@
+﻿using StockHQ.API.Models;
+
+namespace StockHQ.API.Interfaces
+{
+    public interface IInventoryTransactionService
+    {
+        Task AddTransactionAsync(InventoryTransaction transaction);
+
+        Task<IEnumerable<InventoryTransaction>> GetProductHistoryAsync(int productId);
+    }
+}
