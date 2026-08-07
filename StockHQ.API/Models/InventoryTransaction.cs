9e5969b26e542aa48b134c34f24c9580ba619c75
nallicock <nicholasallicock@outlook.com>
2026-08-07 11:04:06 -0400

    added Inventory transaction model to track inv transactions

---
+++ StockHQ.API/Models/InventoryTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockHQ.API.Models
+{
+    public class InventoryTransaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public Product? Product { get; set; }
+
+        [Required]
+        public int QuantityChanged { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Reason { get; set; } = string.Empty;
+
+        public DateTime TransactionDate { get; set; } = DateTime.UtcNow;
+    }
+}
