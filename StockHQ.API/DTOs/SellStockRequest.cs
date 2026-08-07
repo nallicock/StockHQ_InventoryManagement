f29588d58bad8cd1c0ad11dab9ce9ea570fffba6
nallicock <nicholasallicock@outlook.com>
2026-08-07 15:41:59 -0400

    added sellstockrequest dto

---
+++ StockHQ.API/DTOs/SellStockRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockHQ.API.DTOs
+{
+    public class SellStockRequest
+    {
+        [Range(1, 1000000)]
+        public int Quantity { get; set; }
+    }
+}
