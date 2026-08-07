c642b9032edf1d03118b6a4c835f40276a2c298d
nallicock <nicholasallicock@outlook.com>
2026-08-07 13:55:18 -0400

    added receive stock request dto

---
+++ StockHQ.API/DTOs/ReceiveStockRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockHQ.API.DTOs
+{
+    public class ReceiveStockRequest
+    {
+        [Range(1, 1000000)]
+        public int Quantity { get; set; }
+    }
+}
