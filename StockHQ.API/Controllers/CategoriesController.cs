1f18d659a904aad08b861dff1bfdad85d7d50b1f
nallicock <nicholasallicock@outlook.com>
2026-08-06 20:29:50 -0400

    added categories controller

---
+++ StockHQ.API/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockHQ.API.Interfaces;
+using StockHQ.API.Models;
+
+namespace StockHQ.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+
+            return Ok(categories);
+        }
+    }
+}
