d7f7778969213cbdbe17bc02ed500b0c0bda6670
nallicock <nicholasallicock@outlook.com>
2026-08-07 10:52:12 -0400

    added validation to update and create product

--- StockHQ.API/DTOs/CreateProductRequest.cs
+++ StockHQ.API/DTOs/CreateProductRequest.cs
@@ -1,12 +1,25 @@
-﻿namespace StockHQ.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockHQ.API.DTOs
 {
     public class CreateProductRequest
     {
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [StringLength(50)]
         public string SKU { get; set; } = string.Empty;
+
+        [StringLength(500)]
         public string Description { get; set; } = string.Empty;
+
+        [Range(0.01, 1000000)]
         public decimal Price { get; set; }
+
+        [Range(0.01, 1000000)]
         public int QuantityInStock { get; set; }
+
         public int CategoryId { get; set; }
     }
 }
--- StockHQ.API/DTOs/UpdateProductRequest.cs
+++ StockHQ.API/DTOs/UpdateProductRequest.cs
@@ -1,11 +1,24 @@
-﻿namespace StockHQ.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockHQ.API.DTOs
 {
     public class UpdateProductRequest
     {
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [StringLength(50)]
         public string SKU { get; set; } = string.Empty;
+
+        [StringLength(500)]
         public string Description { get; set; } = string.Empty;
+
+        [Range(0.01, 1000000)]
         public decimal Price { get; set; }
+
+        [Range(0.01, 1000000)]
         public int QuantityInStock { get; set; }
         public int CategoryId { get; set; }
     }
