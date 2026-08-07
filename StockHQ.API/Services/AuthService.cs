892b5d95c5d39e4e57c12601061572ffc1d1be1d
nallicock <nicholasallicock@outlook.com>
2026-08-07 19:00:37 -0400

    Auth service added

---
+++ StockHQ.API/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using StockHQ.API.Interfaces;
+using StockHQ.API.Models;
+
+namespace StockHQ.API.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuthService(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> RegisterAsync(string email, string password)
+        {
+            var user = new AppUser
+            {
+                UserName = email,
+                Email = email
+            };
+
+            return await _userManager.CreateAsync(user, password);
+        }
+    }
+}
