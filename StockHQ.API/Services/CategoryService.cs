29144f232e66522697edddeeb250c9aa00d931f4
nallicock <nicholasallicock@outlook.com>
2026-08-06 20:20:13 -0400

    added category service

---
+++ StockHQ.API/Services/CategoryService.cs
@@ -0,0 +1,40 @@
+﻿using StockHQ.API.Interfaces;
+using StockHQ.API.Models;
+
+namespace StockHQ.API.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _repository;
+
+        public CategoryService(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<Category> CreateCategoryAsync(Category category)
+        {
+            return await _repository.AddAsync(category);
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            await _repository.UpdateAsync(category);
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
