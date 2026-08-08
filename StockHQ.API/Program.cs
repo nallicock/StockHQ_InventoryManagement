9a8fd39787ce1169932f8016eea3f78a0cf2ec35
nallicock <nicholasallicock@outlook.com>
2026-08-07 21:13:09 -0400

    implement roles validation/creation

--- StockHQ.API/Program.cs
+++ StockHQ.API/Program.cs
@@ -92,4 +92,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//check if admin and employee roles exist
+//if not, create in aspnetroles table
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roles = { "Admin", "Employee" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
+
 app.Run();
