31cf51f99fb39cc88f2e6f6cc15ed9089c975333
nallicock <nicholasallicock@outlook.com>
2026-08-08 06:34:57 -0400

    added ProductsPage to App.tsx

--- StockHQ.API/StockHQ.Client/src/App.tsx
+++ StockHQ.API/StockHQ.Client/src/App.tsx
@@ -1,8 +1,14 @@
 import ProductsPage from "./pages/ProductsPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-    return <ProductsPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
